80e0bd300594491c7b9dc54a8fcac4180067d349
don <tuneman7>
2023-08-30 22:14:17 -0700

    periodic update

--- lab3_k8/run_o.sh
+++ lab3_k8/run_o.sh
@@ -322,11 +322,11 @@ while [ $running_pods -le $my_all_pods ]; do
     sleep 1
 done
 
-echo my_all_pods=$my_all_pods
-echo running_pods=$running_pods
+echo my_all_pods=$my_all_pods>>log.txt
+echo running_pods=$running_pods>>log.txt
 
-echo "set up the dashboard"
-echo "note this can be done in yaml"
+echo "set up the dashboard">>log.txt
+echo "note this can be done in yaml">>log.txt
 kubectl create serviceaccount k8sadmin -n kube-system
 kubectl create clusterrolebinding k8sadmin --clusterrole=cluster-admin --serviceaccount=kube-system:k8sadmin
 #my_token=kubectl -n kube-system describe secret $(sudo kubectl -n kube-system get secret | (grep k8sadmin || echo "$_") | awk '{print $1}') | grep token: | awk '{print $2}'
@@ -335,19 +335,19 @@ kubectl create clusterrolebinding k8sadmin --clusterrole=cluster-admin --service
 kubectl proxy --address='0.0.0.0' --disable-filter=true>/dev/null &
 proxy_pid=$!
 minikube dashboard --url >/dev/null&
-echo "Open this creature:"
-echo "http://localhost:8001:/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/"
+echo "Open this creature:">>log.txt
+echo "http://localhost:8001:/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/">>log.txt
 
 
 my_ticks=$(( $(date '+%s%N') / 1000000))
 
 sleep 2
 
-echo "*********************************"
-echo "*  BEGIN                        *"
-echo "* port forwarding               *"
-echo "*                               *"
-echo "*********************************"
+echo "*********************************">>log.txt
+echo "*  BEGIN                        *">>log.txt
+echo "* port forwarding               *">>log.txt
+echo "*                               *">>log.txt
+echo "*********************************">>log.txt
 
 echo "kubectl port-forward -n w255 service/frontend 8000:8000 --address='0.0.0.0' > output_$my_ticks.txt &"
 kubectl port-forward -n w255 service/frontend 8000:8000 --address='0.0.0.0' > output_$my_ticks.txt & 

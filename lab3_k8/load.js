e2c7d738eee37e4b01e5e2fceb73a1dc90ec1340
don <tuneman7>
2023-09-17 02:47:38 -0700

    periodic update

--- lab3_k8/load.js
+++ lab3_k8/load.js
@@ -45,7 +45,7 @@ const generator = (cacheRate) => {
 
 }
 
-const NAMESPACE = 'http://192.168.49.2:32342'
+const NAMESPACE = 'http://192.168.49.2:30678'
 const BASE_URL = `${NAMESPACE}`;
 const CACHE_RATE = .43
 
